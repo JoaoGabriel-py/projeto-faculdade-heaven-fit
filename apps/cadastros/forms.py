aa3e0acdc010424d7a420d346319e963d695484b
joaogabriel-py <joao.g.castro20@gmail.com>
2026-04-11 13:11:09 -0300

    fix: input value in modalidade_update.html

--- apps/cadastros/forms.py
+++ apps/cadastros/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import DecimalField, ModelForm
 from .models import Aluno, Modalidade
 
 
@@ -21,6 +21,14 @@ class ModalidadeCreateForm(ModelForm):
         
 
 class ModalidadeUpdateForm(ModelForm):
+    valor = DecimalField(localize=True)
+    
     class Meta:
         model = Modalidade
         fields = ['nome', 'descricao', 'professor', 'valor', 'status']
+        
+    def clean_valor(self):
+        valor = self.cleaned_data.get('valor')
+        if isinstance(valor, str):
+            valor = valor.replace(',', '.')
+        return valor
--- heaven_fitness/settings.py
+++ heaven_fitness/settings.py
@@ -119,6 +119,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_L10N = True
+
+USE_THOUSAND_SEPARATOR = True
+
+DECIMAL_SEPARATOR = ','
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
